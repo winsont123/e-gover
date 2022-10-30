8bf72ab1390414087cce204416b3797f883302e2
winsont123 <winsont.winsont22@gmail.com>
2022-10-30 22:41:47 +0700

    perbaiki layanan

--- script.js
+++ script.js
@@ -4,6 +4,50 @@ ham.addEventListener('click',()=>{
     tautan.classList.toggle('aktif');
     ham.classList.toggle('active');
 })
+let row=document.querySelector('.row');
+// let img= Array.from(document.querySelectorAll('figure img'));
+// img.forEach((a,b)=>{
+//     a.parentElement.parentElement.style.background=`url(${a.getAttribute('img')})`;
+// })
+let layanan=[
+    {
+        layanan:"My Pertamina",
+        gambarBg:"dermaga.jpg",
+        logo:"pertamina.png"
+    },
+    {
+        layanan:"My Pertamina",
+        gambarBg:"dermaga.jpg",
+        logo:"pertamina.png"
+    },
+    {
+        layanan:"My Pertamina",
+        gambarBg:"dermaga.jpg",
+        logo:"pertamina.png"
+    },
+    {
+        layanan:"My Pertamina",
+        gambarBg:"dermaga.jpg",
+        logo:"pertamina.png"
+    },
+    {
+        layanan:"My Pertamina",
+        gambarBg:"dermaga.jpg",
+        logo:"pertamina.png"
+    }
+]
+let tampung ='';
+layanan.forEach((a,b)=>{
+    tampung +=`<div class="card" style="background-image: url('img/${a['gambarBg']}');">
+                    <div class="info">
+                        <div class="layanan">${a['layanan']}</div>
+                        <div class="box">
+                         <img src="img/${a['logo']}" alt="logo pertamina">
+                        </div>
+                    </div>
+                </div>`
+    row.innerHTML=tampung;
+})
 
 
 
