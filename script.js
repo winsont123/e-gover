a71eae2fa4d65ffada7cfb264b824467fa1ffebd
winsont123 <winsont.winsont22@gmail.com>
2022-11-02 22:15:31 +0700

    penambahan berita

--- script.js
+++ script.js
@@ -5,15 +5,27 @@ ham.addEventListener('click',()=>{
     ham.classList.toggle('active');
 })
 let icon=document.querySelector('i');
+let subMenu=document.querySelector('.sub-menu ul');
+let counter =0;
 icon.addEventListener('click',function(){
-    document.querySelector('li ul').classList.toggle('pop');
-    
+    if(counter ==0 ){
+        subMenu.style.display='flex';
+        setTimeout(function(){
+            subMenu.classList.add('pop');
+        },10);
+        counter++;
+    }else if(counter ==1){
+        setTimeout(function(){
+            subMenu.style.display='none';
+        },1000);
+        subMenu.classList.remove('pop');
+        counter--;        
+    }
 })
-let row=document.querySelector('.row');
-// let img= Array.from(document.querySelectorAll('figure img'));
-// img.forEach((a,b)=>{
-//     a.parentElement.parentElement.style.background=`url(${a.getAttribute('img')})`;
-// })
+let row=document.querySelector('section#layanan .row');
+let baris =document.querySelector('section#berita .row');
+
+
 let layanan=[
     {
         layanan:"My Pertamina",
@@ -63,7 +75,18 @@ layanan.forEach((a,b)=>{
                 </div>`
     row.innerHTML=tampung;
 })
-
+let nampung='';
+layanan.forEach((a,b)=>{
+    nampung +=`<div class="kartu" style="background-image: url('img/${a['gambarBg']}');">
+                    <div class="info">
+                        <div class="layanan"><h4>${a['layanan']}</h4></div>
+                        <div class="box">
+                         <img src="img/${a['logo']}" alt="logo pertamina">
+                        </div>
+                    </div>
+                </div>`
+    baris.innerHTML=nampung;
+})
 
 
 
